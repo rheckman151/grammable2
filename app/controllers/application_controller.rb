8cbd2da60b013ae5a98a37f03fd39837589e9dd2
rheckman151 <frisbeeryan151@gmail.com>
2019-09-23 20:02:33 +0000

    alfredo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ApplicationRecord
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
-  
+    protect_from_forgery with: :exception
   
     def render_not_found(status=:not_found)
       render plain: "#{status.to_s.titleize} :(", status: status
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,6 @@
 class GramsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-
+    
     def new
         @gram = Gram.new
     end
